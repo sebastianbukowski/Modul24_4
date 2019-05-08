451ecffa7327a80a74b53f4c4269cc3945edf22e
SebastianBukowski <seb-27@o2.pl>
2019-05-08 20:28:19 +0200

    Add functions to existing tests

--- format-date/index.js
+++ format-date/index.js
@@ -1,8 +1,18 @@
 const formatDate = (timeInSeconds) => {
+  if (!timeInSeconds) timeInSeconds = 0;
+	
+	if (timeInSeconds < 60 ) {
+		return `${timeInSeconds}s`;
+	}
     const hours = Math.floor(timeInSeconds/3600);
     const minutes = Math.floor((timeInSeconds - hours*3600)/60);
     const seconds = timeInSeconds - (hours*3600) - (minutes*60);
-    return hours + 'h '+ minutes + 'm '+ seconds + 's'
+    return [
+      hours ? hours + 'h ' : '',
+      minutes ? minutes + 'm ' : '',
+      seconds ? seconds + 's ' : ''
+    ].join('').trim()
+
   }
    
   module.exports = formatDate;
